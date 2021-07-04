3f82cfb834e7a78db41ab5bae2aee456bab1c940
Joma Simeon <simjoms@gmail.com>
2021-07-04 11:58:43 +0300

    add apollo provider configure apollo client

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const client = new ApolloClient({ cache, link });
 
 const App = () => {
-  return <div>Hello Starwars</div>
-}
-ReactDOM.render(<App />, document.getElementById('app'))
+  return <div>Hello Starwars</div>;
+};
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
